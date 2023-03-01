21d00519db0b73e5c884378ac303905fd1326f6e
taniabg23 <taniabg2023@gmail.com>
2023-03-01 13:02:23 +0100

    Test SearchController

--- src/test/java/co/empathy/academy/search/controllers/SearchControllerTest.java
+++ src/test/java/co/empathy/academy/search/controllers/SearchControllerTest.java
@@ -1,4 +1,26 @@
 package co.empathy.academy.search.controllers;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class SearchControllerTest {
+    @Autowired
+    private MockMvc mvc;
+    @InjectMocks
+    private SearchController searchController;
+
+    @Test
+    void givenQuery_whenSearch_thenReturnsSearchAndClusterName() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/search?query=abc"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect((MockMvcResultMatchers.content().string("{\n\t\"query\": \"abc\",\n\t\"clusterName\": \"docker-cluster\"\n}")));
+    }
 }
