006a3809f6039f08c20990be50f762ce1e631b85
taniabg23 <taniabg2023@gmail.com>
2023-03-09 10:45:18 +0100

    User controller finalizado

---
+++ src/main/java/co/empathy/academy/search/controllers/UserController.java
@@ -0,0 +1,55 @@
+package co.empathy.academy.search.controllers;
+
+import co.empathy.academy.search.model.User;
+import co.empathy.academy.search.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users/list")
+    public ResponseEntity<List<User>> getUsers() {
+        List<User> users = this.userService.getUsers();
+        return ResponseEntity.status(HttpStatus.OK).body(users);
+    }
+
+    @GetMapping("users/details/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        User user = this.userService.getUserById(id);
+        return user == null ?
+                ResponseEntity.status(HttpStatus.NOT_FOUND).build()
+                : ResponseEntity.status(HttpStatus.OK).body(user);
+    }
+
+    @PostMapping("/users/add")
+    public ResponseEntity<User> addUser(@RequestBody User userR) {
+        User user = this.userService.addUser(userR);
+        return user == null ?
+                ResponseEntity.status(HttpStatus.CREATED).body(userR)
+                : ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
+
+    @GetMapping("/users/edit/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userR) {
+        User user = this.userService.updateUser(id, userR);
+        return user == null ?
+                ResponseEntity.status(HttpStatus.NOT_FOUND).build()
+                : ResponseEntity.status(HttpStatus.OK).body(user);
+    }
+
+    @GetMapping("/users/delete/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable Long id) {
+        User user = this.userService.deleteUser(id);
+        return user == null ?
+                ResponseEntity.status(HttpStatus.NOT_FOUND).build()
+                : ResponseEntity.status(HttpStatus.OK).body(user);
+    }
+}
