e92445b45ad9e14ec75329309134549ecd36bf6f
taniabg23 <taniabg2023@gmail.com>
2023-03-09 13:19:16 +0100

    Code for the doc almost finished

--- src/main/java/co/empathy/academy/search/controllers/UserController.java
+++ src/main/java/co/empathy/academy/search/controllers/UserController.java
@@ -2,6 +2,11 @@ package co.empathy.academy.search.controllers;
 
 import co.empathy.academy.search.model.User;
 import co.empathy.academy.search.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,12 +20,20 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Operation(summary = "List all users")
+    @ApiResponse(responseCode = "200", description = "Success")
     @GetMapping("/users/list")
     public ResponseEntity<List<User>> getUsers() {
         List<User> users = this.userService.getUsers();
         return ResponseEntity.status(HttpStatus.OK).body(users);
     }
 
+    @Operation(summary = "Get a user by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "404", description = "Not found")
+    })
+    @Parameter(name = "id", required = true, description = "ID of the user you want to get")
     @GetMapping("users/details/{id}")
     public ResponseEntity<User> getUser(@PathVariable Long id) {
         User user = this.userService.getUserById(id);
@@ -29,6 +42,16 @@ public class UserController {
                 : ResponseEntity.status(HttpStatus.OK).body(user);
     }
 
+    @Operation(summary = "Create a new user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Created"),
+            @ApiResponse(responseCode = "409", description = "Conflict")
+    })
+    @Parameters(value = {
+            @Parameter(name = "id", required = true, description = "ID of the user you want to create"),
+            @Parameter(name = "name", required = true, description = "Name of the user you want to create"),
+            @Parameter(name = "email", required = true, description = "Email of the user you want to create")
+    })
     @PostMapping("/users/add")
     public ResponseEntity<User> addUser(@RequestBody User userR) {
         User user = this.userService.addUser(userR);
@@ -37,14 +60,31 @@ public class UserController {
                 : ResponseEntity.status(HttpStatus.CONFLICT).build();
     }
 
+    @Operation(summary = "Update user's info")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "404", description = "Not found")
+    })
+    @Parameters(value = {
+            @Parameter(name = "idO", required = true, description = "ID of the user's info you want to edit"),
+            @Parameter(name = "id", required = true, description = "New ID for the user "),
+            @Parameter(name = "name", required = true, description = "New name for the user"),
+            @Parameter(name = "email", required = true, description = "New email for the user")
+    })
     @GetMapping("/users/edit/{id}")
-    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userR) {
-        User user = this.userService.updateUser(id, userR);
+    public ResponseEntity<User> updateUser(@PathVariable Long idO, @RequestBody User userR) {
+        User user = this.userService.updateUser(idO, userR);
         return user == null ?
                 ResponseEntity.status(HttpStatus.NOT_FOUND).build()
                 : ResponseEntity.status(HttpStatus.OK).body(user);
     }
 
+    @Operation(summary = "Delete a user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "404", description = "Not found")
+    })
+    @Parameter(name = "id", required = true, description = "ID of the user you want to delete")
     @GetMapping("/users/delete/{id}")
     public ResponseEntity<User> deleteUser(@PathVariable Long id) {
         User user = this.userService.deleteUser(id);
