da5bfdcdbf96bcbeab553b212f34d7b7a93bed5e
taniabg23 <taniabg2023@gmail.com>
2023-03-15 12:02:54 +0100

    Mapping changed and added multipartfile method
    Haven't tried with Postman yet if the multipartfile method works properly

--- src/main/java/co/empathy/academy/search/controllers/UserController.java
+++ src/main/java/co/empathy/academy/search/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -15,13 +16,13 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/users/list")
+    @GetMapping("/users")
     public ResponseEntity<List<User>> getUsers() {
         List<User> users = this.userService.getUsers();
         return ResponseEntity.status(HttpStatus.OK).body(users);
     }
 
-    @GetMapping("users/details/{id}")
+    @GetMapping("users/{id}")
     public ResponseEntity<User> getUserById(@PathVariable Long id) {
         User user = this.userService.getUserById(id);
         return user == null ?
@@ -29,7 +30,7 @@ public class UserController {
                 : ResponseEntity.status(HttpStatus.OK).body(user);
     }
 
-    @PostMapping("/users/add")
+    @PostMapping("/users")
     public ResponseEntity<User> addUser(@RequestBody User userR) {
         User user = this.userService.addUser(userR);
         return user == null ?
@@ -37,7 +38,17 @@ public class UserController {
                 : ResponseEntity.status(HttpStatus.CONFLICT).build();
     }
 
-    @PutMapping("/users/edit/{id}")
+    @PostMapping("/users/addfile")
+    public ResponseEntity<List<User>> addUsersDataFile(@RequestParam MultipartFile file) {
+        try {
+            List<User> users = this.userService.saveUsers(file);
+            return ResponseEntity.status(HttpStatus.OK).body(users);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PutMapping("/users/{id}")
     public ResponseEntity<User> updateUser(@PathVariable Long idO, @RequestBody User userR) {
         User user = this.userService.updateUser(idO, userR);
         return user == null ?
@@ -45,7 +56,7 @@ public class UserController {
                 : ResponseEntity.status(HttpStatus.OK).body(user);
     }
 
-    @DeleteMapping("/users/delete/{id}")
+    @DeleteMapping("/users/{id}")
     public ResponseEntity<User> deleteUser(@PathVariable Long id) {
         User user = this.userService.deleteUser(id);
         return user == null ?
