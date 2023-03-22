1aefc3f964ba4953948a3f1ca9e79cbb778086de
taniabg23 <taniabg2023@gmail.com>
2023-03-22 18:02:53 +0100

    Test for exception handle

--- src/test/java/co/empathy/academy/search/services/UserServiceTest.java
+++ src/test/java/co/empathy/academy/search/services/UserServiceTest.java
@@ -1,5 +1,7 @@
 package co.empathy.academy.search.services;
 
+import co.empathy.academy.search.exceptions.RepeatedUserException;
+import co.empathy.academy.search.exceptions.UserNotFoundException;
 import co.empathy.academy.search.user.model.User;
 import co.empathy.academy.search.user.service.UserServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,8 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class UserServiceTest {
@@ -22,44 +23,44 @@ public class UserServiceTest {
     }
 
     @Test
-    void givenNoUsers_whenAddNewUser_thenReturnsUserData() {
+    void givenNoUsers_whenAddNewUser_thenReturnsUserData() throws RepeatedUserException {
         User user = new User(1L, "user1", "user1@email");
         assertEquals(user, userService.addUser(user));
     }
 
     @Test
-    void givenOneUser_whenAddUserRepeated_thenReturnsNull() {
+    void givenOneUser_whenAddUserRepeated_thenThrowsRepeatedUserException() throws RepeatedUserException {
         User user = new User(2L, "user2", "user2@email");
         assertEquals(user, userService.addUser(user));
-        assertEquals(null, userService.addUser(user));
+        assertThrows(RepeatedUserException.class, () -> userService.addUser(user));
     }
 
     @Test
-    void givenNoUsers_whenGetUserById_thenReturnsNull() {
-        assertEquals(null, userService.getUserById(3L));
+    void givenNoUsers_whenGetUserById_thenThrowsUserNotFoundException() {
+        assertThrows(UserNotFoundException.class, () -> userService.getUserById(3L));
     }
 
     @Test
-    void givenOneUser_whenGetUserById_thenReturnsUsersData() {
+    void givenOneUser_whenGetUserById_thenReturnsUsersData() throws RepeatedUserException, UserNotFoundException {
         User user = new User(4L, "user4", "user4@email");
         assertEquals(user, userService.addUser(user));
         assertEquals(user, userService.getUserById(4L));
     }
 
     @Test
-    void givenNoUser_whenDeleteUserThatDoesntExist_thenReturnsNull() {
-        assertEquals(null, userService.deleteUser(5L));
+    void givenNoUser_whenDeleteUserThatDoesntExist_thenThrowsUserNotFoundException() {
+        assertThrows(UserNotFoundException.class, () -> userService.deleteUser(5L));
     }
 
     @Test
-    void givenOneUser_whenDeleteUser_thenReturnsUsersData() {
+    void givenOneUser_whenDeleteUser_thenReturnsUsersData() throws RepeatedUserException, UserNotFoundException {
         User user = new User(5L, "user5", "user5@email");
         assertEquals(user, userService.addUser(user));
         assertEquals(user, userService.deleteUser(5L));
     }
 
     @Test
-    void givenOneUser_whenUpdateUserExist_thenReturnsNewUser() {
+    void givenOneUser_whenUpdateUserExist_thenReturnsNewUser() throws RepeatedUserException, UserNotFoundException {
         User user = new User(6L, "user6", "user6@email");
         User user2 = new User(6L, "user7", "user7@email");
         assertEquals(user, userService.addUser(user));
@@ -67,13 +68,13 @@ public class UserServiceTest {
     }
 
     @Test
-    void givenNoUser_whenUpdateUserDoesntExist_thenReturnsNull() {
+    void givenNoUser_whenUpdateUserDoesntExist_thenReturnsUserNotFoundException() {
         User user = new User(7L, "user7", "user7@email");
-        assertEquals(null, userService.updateUser(user));
+        assertThrows(UserNotFoundException.class, () -> userService.updateUser(user));
     }
 
     @Test
-    void givenUsers_whenGetAllUsers_thenReturnsAllUsers() {
+    void givenUsers_whenGetAllUsers_thenReturnsAllUsers() throws RepeatedUserException {
         User user = new User(8L, "user8", "user8@email");
         User user2 = new User(9L, "user9", "user9@email");
         assertEquals(user, userService.addUser(user));
